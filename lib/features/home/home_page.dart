c1cb8e5c7408806a3d0c6473476603c449934aa7
Akshit Madan <akmadan1717@gmail.com>
2023-06-15 00:54:33 +0530

    social

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -1,15 +1,8 @@
-import 'dart:io';
-
 import 'package:akshit_madan/features/contact_us/ui/contact_us_widget.dart';
-import 'package:akshit_madan/features/contact_us/ui/desktop/contact_us_desktop_widget.dart';
-import 'package:akshit_madan/features/highlights/ui/desktop/highlights_desktop_widget.dart';
 import 'package:akshit_madan/features/highlights/ui/highlights_widget.dart';
-import 'package:akshit_madan/features/intro/ui/desktop/intro_desktop_widget.dart';
 import 'package:akshit_madan/features/intro/ui/intro_widget.dart';
 import 'package:akshit_madan/features/navbar/navbar_widget.dart';
-import 'package:akshit_madan/features/techstack/ui/desktop/techstack_desktop_widget.dart';
 import 'package:akshit_madan/features/techstack/ui/techstack_widget.dart';
-import 'package:akshit_madan/features/videos/ui/desktop/videos_desktop_widget.dart';
 import 'package:akshit_madan/features/videos/ui/videos_widget.dart';
 import 'package:flutter/material.dart';
 
